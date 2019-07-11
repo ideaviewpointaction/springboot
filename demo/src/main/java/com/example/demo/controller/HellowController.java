91eb79e8e1af1b753c2375a02802f6c81502680b
liumao <liu_mao_yx@163.com>
2019-07-11 21:00:00 +0800

    多环境配置

--- demo/src/main/java/com/example/demo/controller/HellowController.java
+++ demo/src/main/java/com/example/demo/controller/HellowController.java
@@ -1,11 +1,16 @@
 package com.example.demo.controller;
 
+//import ch.qos.logback.classic.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HellowController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Value("${pocket.name}")
     private String name;
@@ -22,7 +27,13 @@ public class HellowController {
     @GetMapping("/hellow")
     public String hellow(){
         String str="hellow, my name is"+name+",I'm"+age+". I live in "+address_liumao;
-        System.out.println(str);
+
+        logger.info(str);
         return str;
     }
+
+
+
+
+
 }
